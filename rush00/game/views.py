bd31dafdba23214ca19c077e2513f835cf977a38
Adrien PARROT <aparrot@e1r6p2.42.fr>
2018-06-02 13:35:30 +0200

    map balise

--- rush00/game/views.py
+++ rush00/game/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here
 
 def init(request):
     return render(request, 'game/django.html')
+
+def coucou(request):
+    #return HttpResponse("coucou")
+    print('coucou')
+    return render(request, 'game/coucou.html')
